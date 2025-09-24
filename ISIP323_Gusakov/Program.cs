9ba9dee5ba9933f1c49bf320dcbca7dcabcb980d
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2025-09-24 11:00:27 +0300

    добавление класса TextSt

--- ISIP323_Gusakov/Program.cs
+++ ISIP323_Gusakov/Program.cs
@@ -1,2 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+
+class TextSt
+{
+    public string Text { get; set; }
+
+    public int WordCount { get; set; }
+    public string ShortesWord { get; set; }
+    public string LongesWord { get; set; }
+    public int SentencCount { get; set; }
+    public int VowelCount { get; set; }
+    public int ConsonantCount { get; set; }
+    public Dictionary<char, int> LetterFreq { get; set; }
+
+    public TextSt(string Text)
+    {
+        Text = Text;
+        LetterFreq = new Dictionary<char, int>();
+    }
+}
+
