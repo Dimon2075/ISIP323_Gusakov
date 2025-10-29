71a87ffd2ab9b8c0111a24c41ac7e010004d319c
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2025-10-29 17:22:14 +0300

    Подключено к бд и сделана декомпозиция

---
+++ ISIP323_Gusakov_Framework1/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ISIP323_Gusakov_Framework1
+{
+    public static class Core
+    {
+        public static AutoServis1Entities4 Context = new AutoServis1Entities4();
+    }
+}
--- ISIP323_Gusakov_Framework1/Program.cs
+++ ISIP323_Gusakov_Framework1/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ISIP323_Gusakov_Framework1
 {
+    public static class GameAuto
+    {
+        private static Random rnd = new Random();
+        public static void ObnPostavok()
+        {
+            
+        }
+        public static void ProcessClientTurn()
+        {
+            
+        }
+        public static void ShowShop()
+        {
+           
+        }
+        public static bool CheckGameOver()
+        {
+           
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Добро пожаловать в Ваш Автосервис!");  
+                while (true)
+                {
+                    GameAuto.ObnPostavok();
+                    GameAuto.ProcessClientTurn();
+                    if (GameAuto.CheckGameOver())
+                    {
+                        break;
+                    }
+                    GameAuto.ShowShop();
+                }
+            Console.WriteLine("Нажмите Enter для выхода...");
+            Console.ReadLine();
         }
     }
 }
